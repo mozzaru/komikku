0ebe7219bd700af9ddd4a0350268398a2abbbcfd
Cuong-Tran <cuongtran.tm@gmail.com>
2025-01-23 17:35:31 +0700

    refactor `domain.test`

--- domain/src/test/java/tachiyomi/domain/episode/service/EpisodeRecognitionTest.kt
+++ domain/src/test/java/tachiyomi/domain/episode/service/EpisodeRecognitionTest.kt
@@ -10,268 +10,268 @@ class EpisodeRecognitionTest {
 
     @Test
     fun `Basic Ch prefix`() {
-        val mangaTitle = "Mokushiroku Alice"
+        val animeTitle = "Mokushiroku Alice"
 
-        assertChapter(mangaTitle, "Mokushiroku Alice Vol.1 Ch.4: Misrepresentation", 4.0)
+        assertEpisode(animeTitle, "Mokushiroku Alice Vol.1 Ch.4: Misrepresentation", 4.0)
     }
 
     @Test
     fun `Basic Ch prefix with space after period`() {
-        val mangaTitle = "Mokushiroku Alice"
+        val animeTitle = "Mokushiroku Alice"
 
-        assertChapter(mangaTitle, "Mokushiroku Alice Vol. 1 Ch. 4: Misrepresentation", 4.0)
+        assertEpisode(animeTitle, "Mokushiroku Alice Vol. 1 Ch. 4: Misrepresentation", 4.0)
     }
 
     @Test
     fun `Basic Ch prefix with decimal`() {
-        val mangaTitle = "Mokushiroku Alice"
+        val animeTitle = "Mokushiroku Alice"
 
-        assertChapter(mangaTitle, "Mokushiroku Alice Vol.1 Ch.4.1: Misrepresentation", 4.1)
-        assertChapter(mangaTitle, "Mokushiroku Alice Vol.1 Ch.4.4: Misrepresentation", 4.4)
+        assertEpisode(animeTitle, "Mokushiroku Alice Vol.1 Ch.4.1: Misrepresentation", 4.1)
+        assertEpisode(animeTitle, "Mokushiroku Alice Vol.1 Ch.4.4: Misrepresentation", 4.4)
     }
 
     @Test
     fun `Basic Ch prefix with alpha postfix`() {
-        val mangaTitle = "Mokushiroku Alice"
+        val animeTitle = "Mokushiroku Alice"
 
-        assertChapter(mangaTitle, "Mokushiroku Alice Vol.1 Ch.4.a: Misrepresentation", 4.1)
-        assertChapter(mangaTitle, "Mokushiroku Alice Vol.1 Ch.4.b: Misrepresentation", 4.2)
-        assertChapter(mangaTitle, "Mokushiroku Alice Vol.1 Ch.4.extra: Misrepresentation", 4.99)
+        assertEpisode(animeTitle, "Mokushiroku Alice Vol.1 Ch.4.a: Misrepresentation", 4.1)
+        assertEpisode(animeTitle, "Mokushiroku Alice Vol.1 Ch.4.b: Misrepresentation", 4.2)
+        assertEpisode(animeTitle, "Mokushiroku Alice Vol.1 Ch.4.extra: Misrepresentation", 4.99)
     }
 
     @Test
     fun `Name containing one number`() {
-        val mangaTitle = "Bleach"
+        val animeTitle = "Bleach"
 
-        assertChapter(mangaTitle, "Bleach 567 Down With Snowwhite", 567.0)
+        assertEpisode(animeTitle, "Bleach 567 Down With Snowwhite", 567.0)
     }
 
     @Test
     fun `Name containing one number and decimal`() {
-        val mangaTitle = "Bleach"
+        val animeTitle = "Bleach"
 
-        assertChapter(mangaTitle, "Bleach 567.1 Down With Snowwhite", 567.1)
-        assertChapter(mangaTitle, "Bleach 567.4 Down With Snowwhite", 567.4)
+        assertEpisode(animeTitle, "Bleach 567.1 Down With Snowwhite", 567.1)
+        assertEpisode(animeTitle, "Bleach 567.4 Down With Snowwhite", 567.4)
     }
 
     @Test
     fun `Name containing one number and alpha`() {
-        val mangaTitle = "Bleach"
+        val animeTitle = "Bleach"
 
-        assertChapter(mangaTitle, "Bleach 567.a Down With Snowwhite", 567.1)
-        assertChapter(mangaTitle, "Bleach 567.b Down With Snowwhite", 567.2)
-        assertChapter(mangaTitle, "Bleach 567.extra Down With Snowwhite", 567.99)
+        assertEpisode(animeTitle, "Bleach 567.a Down With Snowwhite", 567.1)
+        assertEpisode(animeTitle, "Bleach 567.b Down With Snowwhite", 567.2)
+        assertEpisode(animeTitle, "Bleach 567.extra Down With Snowwhite", 567.99)
     }
 
     @Test
-    fun `Chapter containing manga title and number`() {
-        val mangaTitle = "Solanin"
+    fun `Episode containing anime title and number`() {
+        val animeTitle = "Solanin"
 
-        assertChapter(mangaTitle, "Solanin 028 Vol. 2", 28.0)
+        assertEpisode(animeTitle, "Solanin 028 Vol. 2", 28.0)
     }
 
     @Test
-    fun `Chapter containing manga title and number decimal`() {
-        val mangaTitle = "Solanin"
+    fun `Episode containing anime title and number decimal`() {
+        val animeTitle = "Solanin"
 
-        assertChapter(mangaTitle, "Solanin 028.1 Vol. 2", 28.1)
-        assertChapter(mangaTitle, "Solanin 028.4 Vol. 2", 28.4)
+        assertEpisode(animeTitle, "Solanin 028.1 Vol. 2", 28.1)
+        assertEpisode(animeTitle, "Solanin 028.4 Vol. 2", 28.4)
     }
 
     @Test
-    fun `Chapter containing manga title and number alpha`() {
-        val mangaTitle = "Solanin"
+    fun `Episode containing anime title and number alpha`() {
+        val animeTitle = "Solanin"
 
-        assertChapter(mangaTitle, "Solanin 028.a Vol. 2", 28.1)
-        assertChapter(mangaTitle, "Solanin 028.b Vol. 2", 28.2)
-        assertChapter(mangaTitle, "Solanin 028.extra Vol. 2", 28.99)
+        assertEpisode(animeTitle, "Solanin 028.a Vol. 2", 28.1)
+        assertEpisode(animeTitle, "Solanin 028.b Vol. 2", 28.2)
+        assertEpisode(animeTitle, "Solanin 028.extra Vol. 2", 28.99)
     }
 
     @Test
     fun `Extreme case`() {
-        val mangaTitle = "Onepunch-Man"
+        val animeTitle = "Onepunch-Man"
 
-        assertChapter(mangaTitle, "Onepunch-Man Punch Ver002 028", 28.0)
+        assertEpisode(animeTitle, "Onepunch-Man Punch Ver002 028", 28.0)
     }
 
     @Test
     fun `Extreme case with decimal`() {
-        val mangaTitle = "Onepunch-Man"
+        val animeTitle = "Onepunch-Man"
 
-        assertChapter(mangaTitle, "Onepunch-Man Punch Ver002 028.1", 28.1)
-        assertChapter(mangaTitle, "Onepunch-Man Punch Ver002 028.4", 28.4)
+        assertEpisode(animeTitle, "Onepunch-Man Punch Ver002 028.1", 28.1)
+        assertEpisode(animeTitle, "Onepunch-Man Punch Ver002 028.4", 28.4)
     }
 
     @Test
     fun `Extreme case with alpha`() {
-        val mangaTitle = "Onepunch-Man"
+        val animeTitle = "Onepunch-Man"
 
-        assertChapter(mangaTitle, "Onepunch-Man Punch Ver002 028.a", 28.1)
-        assertChapter(mangaTitle, "Onepunch-Man Punch Ver002 028.b", 28.2)
-        assertChapter(mangaTitle, "Onepunch-Man Punch Ver002 028.extra", 28.99)
+        assertEpisode(animeTitle, "Onepunch-Man Punch Ver002 028.a", 28.1)
+        assertEpisode(animeTitle, "Onepunch-Man Punch Ver002 028.b", 28.2)
+        assertEpisode(animeTitle, "Onepunch-Man Punch Ver002 028.extra", 28.99)
     }
 
     @Test
-    fun `Chapter containing dot v2`() {
-        val mangaTitle = "random"
+    fun `Episode containing dot v2`() {
+        val animeTitle = "random"
 
-        assertChapter(mangaTitle, "Vol.1 Ch.5v.2: Alones", 5.0)
+        assertEpisode(animeTitle, "Vol.1 Ch.5v.2: Alones", 5.0)
     }
 
     @Test
-    fun `Number in manga title`() {
-        val mangaTitle = "Ayame 14"
+    fun `Number in anime title`() {
+        val animeTitle = "Ayame 14"
 
-        assertChapter(mangaTitle, "Ayame 14 1 - The summer of 14", 1.0)
+        assertEpisode(animeTitle, "Ayame 14 1 - The summer of 14", 1.0)
     }
 
     @Test
     fun `Space between ch x`() {
-        val mangaTitle = "Mokushiroku Alice"
+        val animeTitle = "Mokushiroku Alice"
 
-        assertChapter(mangaTitle, "Mokushiroku Alice Vol.1 Ch. 4: Misrepresentation", 4.0)
+        assertEpisode(animeTitle, "Mokushiroku Alice Vol.1 Ch. 4: Misrepresentation", 4.0)
     }
 
     @Test
-    fun `Chapter title with ch substring`() {
-        val mangaTitle = "Ayame 14"
+    fun `Episode title with ch substring`() {
+        val animeTitle = "Ayame 14"
 
-        assertChapter(mangaTitle, "Vol.1 Ch.1: March 25 (First Day Cohabiting)", 1.0)
+        assertEpisode(animeTitle, "Vol.1 Ch.1: March 25 (First Day Cohabiting)", 1.0)
     }
 
     @Test
-    fun `Chapter containing multiple zeros`() {
-        val mangaTitle = "random"
+    fun `Episode containing multiple zeros`() {
+        val animeTitle = "random"
 
-        assertChapter(mangaTitle, "Vol.001 Ch.003: Kaguya Doesn't Know Much", 3.0)
+        assertEpisode(animeTitle, "Vol.001 Ch.003: Kaguya Doesn't Know Much", 3.0)
     }
 
     @Test
-    fun `Chapter with version before number`() {
-        val mangaTitle = "Onepunch-Man"
+    fun `Episode with version before number`() {
+        val animeTitle = "Onepunch-Man"
 
-        assertChapter(mangaTitle, "Onepunch-Man Punch Ver002 086 : Creeping Darkness [3]", 86.0)
+        assertEpisode(animeTitle, "Onepunch-Man Punch Ver002 086 : Creeping Darkness [3]", 86.0)
     }
 
     @Test
-    fun `Version attached to chapter number`() {
-        val mangaTitle = "Ansatsu Kyoushitsu"
+    fun `Version attached to episode number`() {
+        val animeTitle = "Ansatsu Kyoushitsu"
 
-        assertChapter(mangaTitle, "Ansatsu Kyoushitsu 011v002: Assembly Time", 11.0)
+        assertEpisode(animeTitle, "Ansatsu Kyoushitsu 011v002: Assembly Time", 11.0)
     }
 
     /**
-     * Case where the chapter title contains the chapter
-     * But wait it's not actual the chapter number.
+     * Case where the episode title contains the episode
+     * But wait it's not actual the episode number.
      */
     @Test
-    fun `Number after manga title with chapter in chapter title case`() {
-        val mangaTitle = "Tokyo ESP"
+    fun `Number after anime title with episode in episode title case`() {
+        val animeTitle = "Tokyo ESP"
 
-        assertChapter(mangaTitle, "Tokyo ESP 027: Part 002: Episode 001", 027.0)
+        assertEpisode(animeTitle, "Tokyo ESP 027: Part 002: Episode 001", 027.0)
     }
 
     /**
-     * Case where the chapter title contains the unwanted tag
-     * But follow by chapter number.
+     * Case where the episode title contains the unwanted tag
+     * But follow by episode number.
      */
     @Test
     fun `Number after unwanted tag`() {
-        val mangaTitle = "One-punch Man"
+        val animeTitle = "One-punch Man"
 
-        assertChapter(mangaTitle, "Mag Version 195.5", 195.5)
+        assertEpisode(animeTitle, "Mag Version 195.5", 195.5)
     }
 
     @Test
-    fun `Unparseable chapter`() {
-        val mangaTitle = "random"
+    fun `Unparseable episode`() {
+        val animeTitle = "random"
 
-        assertChapter(mangaTitle, "Foo", -1.0)
+        assertEpisode(animeTitle, "Foo", -1.0)
     }
 
     @Test
-    fun `Chapter with time in title`() {
-        val mangaTitle = "random"
+    fun `Episode with time in title`() {
+        val animeTitle = "random"
 
-        assertChapter(mangaTitle, "Fairy Tail 404: 00:00", 404.0)
+        assertEpisode(animeTitle, "Fairy Tail 404: 00:00", 404.0)
     }
 
     @Test
-    fun `Chapter with alpha without dot`() {
-        val mangaTitle = "random"
+    fun `Episode with alpha without dot`() {
+        val animeTitle = "random"
 
-        assertChapter(mangaTitle, "Asu No Yoichi 19a", 19.1)
+        assertEpisode(animeTitle, "Asu No Yoichi 19a", 19.1)
     }
 
     @Test
-    fun `Chapter title containing extra and vol`() {
-        val mangaTitle = "Fairy Tail"
+    fun `Episode title containing extra and vol`() {
+        val animeTitle = "Fairy Tail"
 
-        assertChapter(mangaTitle, "Fairy Tail 404.extravol002", 404.99)
-        assertChapter(mangaTitle, "Fairy Tail 404 extravol002", 404.99)
+        assertEpisode(animeTitle, "Fairy Tail 404.extravol002", 404.99)
+        assertEpisode(animeTitle, "Fairy Tail 404 extravol002", 404.99)
     }
 
     @Test
-    fun `Chapter title containing omake (japanese extra) and vol`() {
-        val mangaTitle = "Fairy Tail"
+    fun `Episode title containing omake (japanese extra) and vol`() {
+        val animeTitle = "Fairy Tail"
 
-        assertChapter(mangaTitle, "Fairy Tail 404.omakevol002", 404.98)
-        assertChapter(mangaTitle, "Fairy Tail 404 omakevol002", 404.98)
+        assertEpisode(animeTitle, "Fairy Tail 404.omakevol002", 404.98)
+        assertEpisode(animeTitle, "Fairy Tail 404 omakevol002", 404.98)
     }
 
     @Test
-    fun `Chapter title containing special and vol`() {
-        val mangaTitle = "Fairy Tail"
+    fun `Episode title containing special and vol`() {
+        val animeTitle = "Fairy Tail"
 
-        assertChapter(mangaTitle, "Fairy Tail 404.specialvol002", 404.97)
-        assertChapter(mangaTitle, "Fairy Tail 404 specialvol002", 404.97)
+        assertEpisode(animeTitle, "Fairy Tail 404.specialvol002", 404.97)
+        assertEpisode(animeTitle, "Fairy Tail 404 specialvol002", 404.97)
     }
 
     @Test
-    fun `Chapter title containing commas`() {
-        val mangaTitle = "One Piece"
+    fun `Episode title containing commas`() {
+        val animeTitle = "One Piece"
 
-        assertChapter(mangaTitle, "One Piece 300,a", 300.1)
-        assertChapter(mangaTitle, "One Piece Ch,123,extra", 123.99)
-        assertChapter(mangaTitle, "One Piece the sunny, goes swimming 024,005", 24.005)
+        assertEpisode(animeTitle, "One Piece 300,a", 300.1)
+        assertEpisode(animeTitle, "One Piece Ch,123,extra", 123.99)
+        assertEpisode(animeTitle, "One Piece the sunny, goes swimming 024,005", 24.005)
     }
 
     @Test
-    fun `Chapter title containing hyphens`() {
-        val mangaTitle = "Solo Leveling"
+    fun `Episode title containing hyphens`() {
+        val animeTitle = "Solo Leveling"
 
-        assertChapter(mangaTitle, "ch 122-a", 122.1)
-        assertChapter(mangaTitle, "Solo Leveling Ch.123-extra", 123.99)
-        assertChapter(mangaTitle, "Solo Leveling, 024-005", 24.005)
-        assertChapter(mangaTitle, "Ch.191-200 Read Online", 191.200)
+        assertEpisode(animeTitle, "ch 122-a", 122.1)
+        assertEpisode(animeTitle, "Solo Leveling Ch.123-extra", 123.99)
+        assertEpisode(animeTitle, "Solo Leveling, 024-005", 24.005)
+        assertEpisode(animeTitle, "Ch.191-200 Read Online", 191.200)
     }
 
     @Test
-    fun `Chapters containing season`() {
-        assertChapter("D.I.C.E", "D.I.C.E[Season 001] Ep. 007", 7.0)
+    fun `Episodes containing season`() {
+        assertEpisode("D.I.C.E", "D.I.C.E[Season 001] Ep. 007", 7.0)
     }
 
     @Test
-    fun `Chapters in format sx - chapter xx`() {
-        assertChapter("The Gamer", "S3 - Episode 20", 20.0)
+    fun `Episodes in format sx - episode xx`() {
+        assertEpisode("The Gamer", "S3 - Episode 20", 20.0)
     }
 
     @Test
-    fun `Chapters ending with s`() {
-        assertChapter("One Outs", "One Outs 001", 1.0)
+    fun `Episodes ending with s`() {
+        assertEpisode("One Outs", "One Outs 001", 1.0)
     }
 
     @Test
-    fun `Chapters containing ordinals`() {
-        val mangaTitle = "The Sister of the Woods with a Thousand Young"
+    fun `Episodes containing ordinals`() {
+        val animeTitle = "The Sister of the Woods with a Thousand Young"
 
-        assertChapter(mangaTitle, "The 1st Night", 1.0)
-        assertChapter(mangaTitle, "The 2nd Night", 2.0)
-        assertChapter(mangaTitle, "The 3rd Night", 3.0)
-        assertChapter(mangaTitle, "The 4th Night", 4.0)
+        assertEpisode(animeTitle, "The 1st Night", 1.0)
+        assertEpisode(animeTitle, "The 2nd Night", 2.0)
+        assertEpisode(animeTitle, "The 3rd Night", 3.0)
+        assertEpisode(animeTitle, "The 4th Night", 4.0)
     }
 
-    private fun assertChapter(mangaTitle: String, name: String, expected: Double) {
-        EpisodeRecognition.parseEpisodeNumber(mangaTitle, name) shouldBe expected
+    private fun assertEpisode(animeTitle: String, name: String, expected: Double) {
+        EpisodeRecognition.parseEpisodeNumber(animeTitle, name) shouldBe expected
     }
 }
