76bb77fb112a4eeffd6f467cc6c0687864f8ebaa
mozzaru <rianyanuar52@gmail.com>
2026-04-20 08:31:12 +0700

    build mozzaru

--- app/src/main/java/eu/kanade/tachiyomi/data/updater/AppUpdateChecker.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/updater/AppUpdateChecker.kt
@@ -97,9 +97,9 @@ val GITHUB_REPO: String by lazy { getGithubRepo() }
 
 fun getGithubRepo(peekIntoPreview: Boolean = false): String =
     if (isPreviewBuildType || peekIntoPreview) {
-        "komikku-app/komikku-preview"
+        "mozzaru/komikku-preview"
     } else {
-        "komikku-app/komikku"
+        "mozzaru/komikku"
     }
 
 val RELEASE_TAG: String by lazy { getReleaseTag() }
